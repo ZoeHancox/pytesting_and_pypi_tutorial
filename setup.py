c0297566633c1742909f5f33eec1d36ee0a9e7fe
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-06-27 12:47:10 +0100

    removed long decript, added short descript

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r") as f:
-    description = f.read()
-
 with open("requirements.txt", "r") as f:
     install_requirements = f.read().splitlines()
 
@@ -12,6 +9,5 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=install_requirements,
-    long_description=description,
-    long_description_content_type="text/markdown",
+    description= "Produce a password of length x."
 )
